a4bd0bde897bec724d83eb729f21f4a6ad1dd29e
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-11 01:32:27 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
+insert into bears (name, age, gender, color, temperament, alive) values 
