7f02722596d76ef6ad8348f8e96e40f479e9f312
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-11 01:55:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 5, 'M', 'black', 'calm', 0)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 3, 'M', 'brown', 'feral', 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,"Tabitha" 1, 'F', 'black', 'sweet', 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"Sergeant Brown", 4, 'M', 'brown', 'territorial', 0)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 2, 'F', 'black', 'calm', 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 4, 'M', 'green', 'feral', 0)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 5, 'F', 'black', 'territorial', 0)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 5, 'M', 'black', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 3, 'M', 'brown', 'feral', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,"Tabitha" 1, 'F', 'black', 'sweet', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"Sergeant Brown", 4, 'M', 'brown', 'territorial', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 2, 'F', 'black', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 4, 'M', 'green', 'feral', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 5, 'F', 'black', 'territorial', 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,NULL, 1, 'F', 'black', 'shy', 1);
-,
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0)
