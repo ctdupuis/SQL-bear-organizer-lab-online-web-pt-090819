aaceb4f0a46fa1abb0e99726460b798cda5f2f68
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-11 02:03:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,13 +1,13 @@
 def selects_all_female_bears_return_name_and_age
-  "select bears.name, bears.age from bears where gender='F'"
+  "select bears.name, bears.age from bears where gender='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "select bears.name from bears order by name asc"
+  "select bears.name from bears order by name asc;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "select bears.name, bears.age from bears where alive=1 order by age desc"
+  "select bears.name, bears.age from bears where alive=1 order by bears.age desc;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
