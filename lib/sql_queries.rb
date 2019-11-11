0107a9daf823c72fc6d512a62dee433ed190cf9e
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-11 02:05:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "select bears.name, bears.age from bears where "
+  "select bears.name, bears.age from bears order by bears.age desc limit 1"
 end
 
 def select_youngest_bear_and_returns_name_and_age
